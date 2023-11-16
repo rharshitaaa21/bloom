c6bc59d40648891a47b3dcfaf78a6aa9bddcfa55
rharshitaaa21 <raghav.harshita21@gmail.com>
2023-11-16 23:27:15 +0530

    stacked product images

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from base.models import BaseModel
+from django.utils.text import slugify
 # Create your models here.
 
 
@@ -9,6 +10,13 @@ class Category(BaseModel):
     slug = models.SlugField(unique=True , null=True , blank=True)
     category_image = models.ImageField(upload_to="catgories")
 
+    def save(self,  *args, **kwargs):
+        self.slug = slugify(self.category_name)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.category_name
+    
 
 class Product(BaseModel):
     product_name = models.CharField(max_length=100)
@@ -16,6 +24,14 @@ class Product(BaseModel):
     category = models.ForeignKey(Category , on_delete=models.CASCADE , related_name="products")
     price = models.IntegerField()
     product_desription = models.TextField()
+
+    def save(self,  *args, **kwargs):
+        self.slug = slugify(self.product_name)
+        super(Product, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.product_name
+    
 
 
 class ProductImage(BaseModel):
--- products/views.py
+++ products/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse  # Import HttpResponse
+from django.http import HttpResponse
+
+from products.models import Product  # Import HttpResponse
 
 def get_product(request, slug):
 
     product = f"Product with slug: {slug}"
+    # product = Product.objects.get(slug =slug)
 
     return render(request, 'product/product.html', {'product': product})
